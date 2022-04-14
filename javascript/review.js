5c4f95d0da915304be288ae28b9de2baabd14b02
Miran Ul Haq <68357661+mnuh96@users.noreply.github.com>
2022-04-14 07:39:22 -0400

    1st Upload of the Project

---
+++ javascript/review.js
@@ -0,0 +1,50 @@
+function searchcompany() {
+    var s = document.getElementById('add').value;
+    s.addEventListener("keyup", function(event) {
+     if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("btn").click();
+     }
+   });
+    document.getElementById("company").innerHTML = s;
+ }
+ 
+ 
+ function prompterr(){
+     alert("Blog Not available yet! Stay Tuned...");
+ }
+ function promptmsg(){
+     alert("We'll Contact You Soon");
+ }
+ function gotohome(){
+     location.replace("index.html")
+ }
+ function gotoreview(){
+     location.replace("review.html")
+ }
+ function gotobrowsecategories(){
+     location.replace("browsecategories.html")
+ }
+ function gotoblog(){
+     location.replace("blog.html")
+ }
+ function gotologin(){
+     location.replace("login.html")
+ }
+ function gotosignup(){
+     location.replace("signup.html")
+ }
+ function gotocontact(){
+     location.replace("contact.html")
+ }
+ function gotoforpass(){
+     location.replace("forpass.html")
+ }
+ function openForm() {
+   document.getElementById("myForm").style.display = "block";
+ }
+ 
+ function closeForm() {
+   document.getElementById("myForm").style.display = "none";
+ }
+ 
